75c059864a1d0fbf47a02668cc658a1db9b7c5de
Aiden Bradley <116838707+AidenBradley24@users.noreply.github.com>
2024-06-25 17:28:13 -0400

    bad code

--- src/Server.cs
+++ src/Server.cs
@@ -1,6 +1,16 @@
 using System.Net;
 using System.Net.Sockets;
+using System.Text;
 
 TcpListener server = new(IPAddress.Any, 6379);
 server.Start();
-server.AcceptSocket(); // wait for client
+TcpClient client = server.AcceptTcpClient();
+
+NetworkStream ns = client.GetStream();
+byte[] buffer = new byte[1024];
+ns.Read(buffer);
+
+if (Encoding.ASCII.GetString(buffer).Contains("PING"))
+{
+    ns.Write(Encoding.ASCII.GetBytes("+PONG\r\n"));
+}
