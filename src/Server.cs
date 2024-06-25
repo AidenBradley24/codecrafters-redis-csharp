cee64469f6d4c6eb5d03ab894ae1087170567019
Aiden Bradley <116838707+AidenBradley24@users.noreply.github.com>
2024-06-25 17:43:16 -0400

    more pings

--- src/Server.cs
+++ src/Server.cs
@@ -4,13 +4,23 @@ using System.Text;
 
 TcpListener server = new(IPAddress.Any, 6379);
 server.Start();
-TcpClient client = server.AcceptTcpClient();
 
-NetworkStream ns = client.GetStream();
-byte[] buffer = new byte[1024];
-ns.Read(buffer);
-
-if (Encoding.ASCII.GetString(buffer).Contains("PING"))
+while (true)
 {
-    ns.Write(Encoding.ASCII.GetBytes("+PONG\r\n"));
+    TcpClient client = server.AcceptTcpClient();
+    _ = Task.Run(async () => await HandleClient(client));
+}
+
+static Task HandleClient(TcpClient client)
+{
+    NetworkStream ns = client.GetStream();
+    byte[] buffer = new byte[1024];
+    ns.Read(buffer);
+
+    if (Encoding.ASCII.GetString(buffer).Contains("PING"))
+    {
+        ns.Write(Encoding.ASCII.GetBytes("+PONG\r\n"));
+    }
+
+    return Task.CompletedTask;
 }
