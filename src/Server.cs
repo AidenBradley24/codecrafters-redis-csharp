b4e85dcb2b0a929a1404b6ad712dc47601dc6ba1
Aiden Bradley <116838707+AidenBradley24@users.noreply.github.com>
2024-06-28 21:00:03 -0400

    test

--- src/Server.cs
+++ src/Server.cs
@@ -58,124 +58,128 @@ Task HandleClient(TcpClient client, bool clientIsMaster)
     {
         using RedisReader rr = InitRead(ns, buffer);
         object[] request = (object[])rr.ReadAny();
-
-        bool HasArgument(string arg, int index)
-        {
-            return request.Length > index && ((string)request[index]).Equals(arg, StringComparison.InvariantCultureIgnoreCase);
-        }
-
-        Console.WriteLine($"recieved request:");
-        foreach (object obj in request)
-        {
-            Console.WriteLine(obj);
-        }
-        Console.WriteLine("(end of request)");
-
-        string command = ((string)request[0]).ToUpperInvariant();
-
-        if (propagatedCommands.Contains(command))
-        {
-            foreach (TcpClient repClient in myReplicas)
-            {
-                try
-                {
-                    NetworkStream masterConnection = repClient.GetStream();
-                    RedisWriter writer = new(masterConnection);
-                    writer.WriteArray(request);
-                    Console.WriteLine($"command replicated to {repClient}");
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine(ex.ToString());
-                }
-            }
-        }
-
         RedisWriter rw = new(ns) { Enabled = !clientIsMaster };
-        switch (command)
-        {
-            case "PING":
-                rw.WriteSimpleString("PONG");
-                break;
-            case "ECHO":
-                rw.WriteBulkString((string)request[1]);
-                break;
-            case "GET":
-                {
-                    (string val, DateTime? timeout) dat;
-                    bool hasVal = false;
-                    lock (myCache)
-                    {
-                        hasVal = myCache.TryGetValue((string)request[1], out dat);
-                    }
-
-                    if (!hasVal)
-                    {
-                        rw.WriteBulkString(null);
-                    }
-                    else if(dat.timeout != null && DateTime.Now >= dat.timeout)
-                    {
-                        myCache.Remove((string)request[1]);
-                        rw.WriteBulkString(null);
-                    }
-                    else
-                    {
-                        rw.WriteBulkString(dat.val);
-                    }
-                }
-                break;
-            case "SET":
-                {
-                    DateTime? timeout = null;
-                    if (HasArgument("px", 3))
-                    {
-                        int milliseconds = Convert.ToInt32(request[4]);
-                        timeout = DateTime.Now.AddMilliseconds(milliseconds);
-                    }
-
-                    lock (myCache)
-                    {
-                        myCache[(string)request[1]] = ((string)request[2], timeout);
-                    }
-
-                    rw.WriteSimpleString("OK");
-                }
-                break;
-            case "INFO":
-                {
-                    StringBuilder sb = new();
-                    foreach(KeyValuePair<string, object> pair in myInfo)
-                    {
-                        sb.Append(pair.Key);
-                        sb.Append(':');
-                        sb.Append(pair.Value);
-                        sb.Append('\n');
-                    }
-                    rw.WriteBulkString(sb.ToString());
-                }
-                break;
-            case "REPLCONF":
-                {
-                    if (HasArgument("listening-port", 1))
-                    {
-                        int port = Convert.ToInt32(request[2]);
-                        Console.WriteLine($"adding client: {port}");
-                    }
-
-                    rw.WriteSimpleString("OK");
-                }
-                break;
-            case "PSYNC":
-                rw.WriteSimpleString($"FULLRESYNC {myInfo["master_replid"]} {myInfo["master_repl_offset"]}");
-                rw.WriteEmptyRDB(); // TODO write actual db
-                myReplicas.Add(client);
-                break;
-        }
+        HandleRequest(request, rw, client);
     }
 
     Console.WriteLine($"closing connection: {client.Client.RemoteEndPoint}");
     client.Dispose();
     return Task.CompletedTask;
+}
+
+void HandleRequest(object[] request, RedisWriter rw, TcpClient client)
+{
+    bool HasArgument(string arg, int index)
+    {
+        return request.Length > index && ((string)request[index]).Equals(arg, StringComparison.InvariantCultureIgnoreCase);
+    }
+
+    Console.WriteLine($"recieved request:");
+    foreach (object obj in request)
+    {
+        Console.WriteLine(obj);
+    }
+    Console.WriteLine("(end of request)");
+
+    string command = ((string)request[0]).ToUpperInvariant();
+
+    if (propagatedCommands.Contains(command))
+    {
+        foreach (TcpClient repClient in myReplicas)
+        {
+            try
+            {
+                NetworkStream masterConnection = repClient.GetStream();
+                RedisWriter writer = new(masterConnection);
+                writer.WriteArray(request);
+                Console.WriteLine($"command replicated to {repClient}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+    }
+
+    switch (command)
+    {
+        case "PING":
+            rw.WriteSimpleString("PONG");
+            break;
+        case "ECHO":
+            rw.WriteBulkString((string)request[1]);
+            break;
+        case "GET":
+            {
+                (string val, DateTime? timeout) dat;
+                bool hasVal = false;
+                lock (myCache)
+                {
+                    hasVal = myCache.TryGetValue((string)request[1], out dat);
+                }
+
+                if (!hasVal)
+                {
+                    rw.WriteBulkString(null);
+                }
+                else if (dat.timeout != null && DateTime.Now >= dat.timeout)
+                {
+                    myCache.Remove((string)request[1]);
+                    rw.WriteBulkString(null);
+                }
+                else
+                {
+                    rw.WriteBulkString(dat.val);
+                }
+            }
+            break;
+        case "SET":
+            {
+                DateTime? timeout = null;
+                if (HasArgument("px", 3))
+                {
+                    int milliseconds = Convert.ToInt32(request[4]);
+                    timeout = DateTime.Now.AddMilliseconds(milliseconds);
+                }
+
+                lock (myCache)
+                {
+                    myCache[(string)request[1]] = ((string)request[2], timeout);
+                }
+
+                rw.WriteSimpleString("OK");
+            }
+            break;
+        case "INFO":
+            {
+                StringBuilder sb = new();
+                foreach (KeyValuePair<string, object> pair in myInfo)
+                {
+                    sb.Append(pair.Key);
+                    sb.Append(':');
+                    sb.Append(pair.Value);
+                    sb.Append('\n');
+                }
+                rw.WriteBulkString(sb.ToString());
+            }
+            break;
+        case "REPLCONF":
+            {
+                if (HasArgument("listening-port", 1))
+                {
+                    int port = Convert.ToInt32(request[2]);
+                    Console.WriteLine($"adding client: {port}");
+                }
+
+                rw.WriteSimpleString("OK");
+            }
+            break;
+        case "PSYNC":
+            rw.WriteSimpleString($"FULLRESYNC {myInfo["master_replid"]} {myInfo["master_repl_offset"]}");
+            rw.WriteEmptyRDB(); // TODO write actual db
+            myReplicas.Add(client);
+            break;
+    }
 }
 
 static RedisReader InitRead(NetworkStream ns, byte[] buffer)
@@ -237,54 +241,6 @@ void StartReplica()
     {
         RedisReader rr = InitRead(ns, buffer);
         // recieving FULLRESYNC here
-    }
-
-    {
-        RedisReader rr = InitRead(ns, buffer);
-        object response = rr.ReadAny();
-        if (response is object[] array)
-        {
-            foreach (object o in array)
-            {
-                Console.WriteLine(o.ToString());
-            }
-        }
-        else
-        {
-            Console.WriteLine(response);
-        }
-    }
-
-    {
-        RedisReader rr = InitRead(ns, buffer);
-        object response = rr.ReadAny();
-        if (response is object[] array)
-        {
-            foreach (object o in array)
-            {
-                Console.WriteLine(o.ToString());
-            }
-        }
-        else
-        {
-            Console.WriteLine(response);
-        }
-    }
-
-    {
-        RedisReader rr = InitRead(ns, buffer);
-        object response = rr.ReadAny();
-        if (response is object[] array)
-        {
-            foreach (object o in array)
-            {
-                Console.WriteLine(o.ToString());
-            }
-        }
-        else
-        {
-            Console.WriteLine(response);
-        }
     }
 
     Console.WriteLine("handshake 4/4");
